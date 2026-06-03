b8687bbf577a62372e38a27cbc76d5e857397701
Kevin More Sandoval <kevinjesusmoresandoval@gmail.com>
2026-06-03 15:27:18 -0500

    first commit

---
+++ src/main/java/curso_integrador/easylab_backend/EasylabBackendApplication.java
@@ -0,0 +1,13 @@
+package curso_integrador.easylab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasylabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curso_integrador/easylab_backend/EasylabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package curso_integrador.easylab_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasylabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
